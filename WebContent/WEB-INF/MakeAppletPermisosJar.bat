ba6a47b6cb40ef67f6344a99a932d798ad821c59
raul <papelera@redabogacia.org>
2009-10-21 13:13:47 +0000

    *** empty log message ***

---
+++ WebContent/WEB-INF/MakeAppletPermisosJar.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+cls
+cd classes
+jar cvf "C:/eclipse/workspace/SIGA92/WebContent/html/jsp/administracion/processTree/AppletPermisos.jar" "com/siga/gui/processTree" "com/siga/administracion/SIGAConstants.class"
+PAUSE
---
+++ WebContent/WEB-INF/MakeAppletPermisosJarAppletIP.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+cls
+jar cvf "C:/eclipse/workspace/SIGA92/WebContent/html/jsp/general/AppletDireccionIP.jar" "com/siga/Utilidades/AppletDireccionIP.class"
+PAUSE
