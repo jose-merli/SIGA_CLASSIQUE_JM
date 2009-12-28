54c5f41a2377ce10db3fcefd4d72662b72b701e2
Angel Corral Perez <angel.corral@redabogacia.org>
2009-12-28 10:21:34 +0000

    Clase para ejecutar WS de manera offline

---
+++ src/com/siga/ws/SIGAWSListener.java
@@ -0,0 +1,103 @@
+package com.siga.ws;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.management.InstanceNotFoundException;
+import javax.management.Notification;
+import javax.management.NotificationListener;
+
+import weblogic.management.timer.Timer;
+
+import com.atos.utils.ClsLogging;
+
+public class SIGAWSListener implements NotificationListener {
+	
+	private Integer idNotificacion;
+	
+	/**
+	 * Lista para guardar las remesas que se estan ejecutando
+	 */
+	private static List<String> ejecutandose = new ArrayList<String>();
+		
+	
+	/**
+	 * Metodo encargado de ejecutar nuestra clase de negocio
+	 */
+	public void handleNotification(Notification notification, Object handback) {
+		
+		String key = null;
+		SIGAWSClientAbstract sigaWSClient = null;
+		
+		try {
+			Object obj = notification.getUserData();
+			if (obj == null || !(obj instanceof SIGAWSClientAbstract)) {
+				throw new IllegalArgumentException("El objeto UserData es nulo o no es del tipo esperado");
+			}			
+			
+			sigaWSClient = (SIGAWSClientAbstract) obj;
+			key = getKey(sigaWSClient.getIdInstitucion(), sigaWSClient.getIdRemesa());
+			ejecutandose.add(key);			
+			sigaWSClient.execute();
+			
+			// paramos la ejecución
+			if (handback != null && handback instanceof Timer) {
+				Timer timer = (Timer) handback;
+				try {
+					timer.removeNotification(idNotificacion);
+				} catch (InstanceNotFoundException e) {
+					ClsLogging.writeFileLogError("Error en el timer de " + this.getClass(), e, 3);
+				}
+				if (timer.isActive()) {
+					timer.stop();
+				}
+			}
+
+		} catch (Exception e) {
+			ClsLogging.writeFileLogError("ERROR en el envío WebService", e, 3);			
+		} finally {
+			if (key != null) {
+				ejecutandose.remove(key);
+			}
+			if (sigaWSClient != null) {
+				sigaWSClient.closeLogFile();
+			}
+		}
+	}
+	
+	/**
+	 * Metodo para saber si se esta ejecutando una determinada remesa de una institucion
+	 * @param idInstitucion
+	 * @param idRemesa
+	 * @return
+	 */
+	public static boolean isEjecutandose(int idInstitucion, int idRemesa) {
+		String keyMap = getKey(idInstitucion, idRemesa);
+		return ejecutandose.contains(keyMap);
+	}
+
+	/**
+	 * Para construir el objeto key que se meterá en la lista de ejecutandose
+	 * @param idInstitucion
+	 * @param idRemesa
+	 * @return
+	 */
+	private static String getKey(int idInstitucion, int idRemesa) {		
+		return idInstitucion + "-" + idRemesa;
+	}
+
+	/**
+	 * @return the idNotificacion
+	 */
+	public Integer getIdNotificacion() {
+		return idNotificacion;
+	}
+
+	/**
+	 * @param idNotificacion the idNotificacion to set
+	 */
+	public void setIdNotificacion(Integer idNotificacion) {
+		this.idNotificacion = idNotificacion;
+	}
+
+}
