a0626a454f5303516ede74c740476d5d6f6c5732
Angel Corral Perez <angel.corral@redabogacia.org>
2009-12-28 09:24:27 +0000

    Bean de la tabla CAJG_RESPUESTA_EJGREMESA

---
+++ src/com/siga/beans/CajgRespuestaEJGRemesaBean.java
@@ -0,0 +1,111 @@
+package com.siga.beans;
+
+/**
+ * Implementa las operaciones sobre el bean de la tabla CAJG_RESPUESTA_EJGREMESA
+ * 
+ * @author angel corral
+ * @since 17/09/2008
+ */
+
+public class CajgRespuestaEJGRemesaBean extends MasterBean{
+	
+	/*
+	 *  Variables */ 
+	private Integer idRespuesta;
+	private Integer idEjgRemesa;
+	private String codigo;
+	private String descripcion;
+	private String abreviatura;
+	private String fecha;
+	
+	
+	
+	/*
+	 *  Nombre de Tabla*/
+	
+	
+	static public String T_NOMBRETABLA = "CAJG_RESPUESTA_EJGREMESA";
+	
+	/*
+	 * Nombre de campos de la tabla*/
+	static public final String C_IDRESPUESTA = "IDRESPUESTA";
+	static public final String C_IDEJGREMESA = "IDEJGREMESA"; 
+	static public final String C_CODIGO = "CODIGO";
+	static public final String C_DESCRIPCION = "DESCRIPCION"; 
+	static public final String C_ABREVIATURA = "ABREVIATURA";
+	static public final String C_FECHA = "FECHA";
+	/**
+	 * @return the idRespuesta
+	 */
+	public Integer getIdRespuesta() {
+		return idRespuesta;
+	}
+	/**
+	 * @param idRespuesta the idRespuesta to set
+	 */
+	public void setIdRespuesta(Integer idRespuesta) {
+		this.idRespuesta = idRespuesta;
+	}
+	/**
+	 * @return the idEjgRemesa
+	 */
+	public Integer getIdEjgRemesa() {
+		return idEjgRemesa;
+	}
+	/**
+	 * @param idEjgRemesa the idEjgRemesa to set
+	 */
+	public void setIdEjgRemesa(Integer idEjgRemesa) {
+		this.idEjgRemesa = idEjgRemesa;
+	}
+	/**
+	 * @return the codigo
+	 */
+	public String getCodigo() {
+		return codigo;
+	}
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	/**
+	 * @return the descripcion
+	 */
+	public String getDescripcion() {
+		return descripcion;
+	}
+	/**
+	 * @param descripcion the descripcion to set
+	 */
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	/**
+	 * @return the abreviatura
+	 */
+	public String getAbreviatura() {
+		return abreviatura;
+	}
+	/**
+	 * @param abreviatura the abreviatura to set
+	 */
+	public void setAbreviatura(String abreviatura) {
+		this.abreviatura = abreviatura;
+	}
+	/**
+	 * @return the fecha
+	 */
+	public String getFecha() {
+		return fecha;
+	}
+	/**
+	 * @param fecha the fecha to set
+	 */
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+		
+	
+}
