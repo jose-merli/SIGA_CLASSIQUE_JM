940fce2e0f7ba42a66fd87fe1dbba41ce538b8f7
Jose Barrientos Diaz <jose.barrientos@redabogacia.org>
2010-06-01 08:21:12 +0000

    inc7222: Recuperado el paginador

--- src/com/siga/gratuita/action/DefinirExpedientesSOJAction.java
+++ src/com/siga/gratuita/action/DefinirExpedientesSOJAction.java
@@ -98,7 +98,7 @@ public class DefinirExpedientesSOJAction extends MasterAction {
 		HashMap databackup=new HashMap();
 
 		//Realiza la recuperación de los datos.
-	   if ((request.getSession().getAttribute("DATAPAGINADOR")!=null)||(request.getSession().getAttribute("DATAPAGINADOR")==null)){		   
+	   if ((request.getSession().getAttribute("DATAPAGINADOR")!=null)){		   
 	 	databackup = (HashMap)request.getSession().getAttribute("DATAPAGINADOR");
 			     PaginadorBind paginador = (PaginadorBind)databackup.get("paginador");
 			     Vector datos=new Vector();
