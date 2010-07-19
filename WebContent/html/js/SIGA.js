c178b78b604ec6704f4d550a6bdd1da53cd200be
Jorge Torres Acosta <jorge.torres@redabogacia.org>
2010-07-19 09:51:06 +0000

    +findPosY +findPosX

--- WebContent/html/js/SIGA.js
+++ WebContent/html/js/SIGA.js
@@ -1202,3 +1202,36 @@ function ocultarDIV(ident){
 	ajusteAlto('mainWorkarea');
 	return true;
 }
+function findPosY(obj)
+  {
+    var curtop = 0;
+    if(obj.offsetParent)
+        while(1)
+        {
+          curtop += obj.offsetTop;
+          if(!obj.offsetParent)
+            break;
+          obj = obj.offsetParent;
+        }
+    else if(obj.y)
+        curtop += obj.y;
+    return curtop;
+  }
+  function findPosX(obj)
+  {
+    var curleft = 0;
+    if(obj.offsetParent)
+        while(1) 
+        {
+          curleft += obj.offsetLeft;
+          if(!obj.offsetParent)
+            break;
+          obj = obj.offsetParent;
+        }
+    else if(obj.x)
+        curleft += obj.x;
+    return curleft;
+  }
+  
+
+
