3f482711e6461e108b309b7bfc5083a0bf928c8e
Angel Corral Perez <angel.corral@redabogacia.org>
2010-08-26 10:52:14 +0000

    *** empty log message ***

--- src/com/siga/ws/cat/PCAJGxmlResponse.java
+++ src/com/siga/ws/cat/PCAJGxmlResponse.java
@@ -481,7 +481,6 @@ public class PCAJGxmlResponse extends SIGAWSClientAbstract implements PCAJGConst
 			}
 
 		} catch (Exception e) {
-			e.printStackTrace();
 			if (file != null) {
 				file.delete();
 			}
