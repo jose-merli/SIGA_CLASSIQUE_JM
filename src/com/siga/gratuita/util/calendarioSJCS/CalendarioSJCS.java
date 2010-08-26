28eec3cf60ed8b388e3b3e6506415eadcc733822
Jose Barrientos Diaz <jose.barrientos@redabogacia.org>
2010-08-26 08:48:03 +0000

    *** empty log message ***

--- src/com/siga/gratuita/util/calendarioSJCS/CalendarioSJCS.java
+++ src/com/siga/gratuita/util/calendarioSJCS/CalendarioSJCS.java
@@ -737,8 +737,8 @@ public class CalendarioSJCS {
 				idInstitucion, idTurno, idGuardia,
 				letradoGuardia.getIdPersona(), saltoOCompensacion);
 		
-		//saltoCompensacion.setFechaCumplimiento((String) diasGuardia.get(0));
-		saltoCompensacion.setFechaCumplimiento(GstDate.getApplicationFormatDate("", (String) diasGuardia.get(0)) );
+		saltoCompensacion.setFechaCumplimiento((String) diasGuardia.get(0));
+		//saltoCompensacion.setFechaCumplimiento(GstDate.getApplicationFormatDate("", (String) diasGuardia.get(0)) );
 		saltoCompensacion.setIdCalendarioGuardias(idCalendarioGuardias);
 		saltoCompensacion.setMotivos(motivo);
 		
