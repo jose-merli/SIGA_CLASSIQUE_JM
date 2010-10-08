fdebb4b2f9e6550caecbafc97777c9ba14f793e2
yacky <papelera@redabogacia.org>
2010-10-08 11:43:15 +0000

    Se borra el codigoexterno Juzgado.

--- src/com/siga/beans/ScsDesignaAdm.java
+++ src/com/siga/beans/ScsDesignaAdm.java
@@ -1683,7 +1683,7 @@ public class ScsDesignaAdm extends MasterBeanAdministrador {
 				else
 					registro.put("DESC_TIPODESIGNA", " ");
 				
-               
+                
 				
 				//metemos el numero de colegiado
 				if(registro.containsKey("IDPERSONA") && registro.get("IDPERSONA")!=null && !((String)registro.get("IDPERSONA")).trim().equals("")){
@@ -1780,40 +1780,12 @@ public class ScsDesignaAdm extends MasterBeanAdministrador {
 				helperInformes.completarHashSalida(registro,getActuacionDesignaSalidaOficio(idInstitucion,numeroDesigna,idTurno,anioDesigna));
 				
 				String idJuzgado = (String)registro.get("IDJUZGADO");
-				
 				String idInstJuzgado = (String)registro.get("IDINSTITUCION_JUZG");
 				
 				if(idJuzgado==null ||idJuzgado.trim().equalsIgnoreCase("")){
 				    idJuzgado="-33"; // forzamos que no encuentre datos, en lugar de dar error
 				}
-				String codigoExtJuzgado="";
-				if (!idJuzgado.equals("-33")){
-					UsrBean usrBean= new UsrBean();
-					ScsJuzgadoAdm JuzgadoAdm = new ScsJuzgadoAdm (this.usrbean);			
-					 codigoExtJuzgado=JuzgadoAdm.obtenerCodigoExtJuzgado(idInstitucion, idJuzgado.toString());
-					 registro.put("CODIGOJUZGADO",codigoExtJuzgado);
-				}else{
-					 registro.put("CODIGOJUZGADO",codigoExtJuzgado);
-				}
-			
 				
-				 String Idpretension  = (String)registro.get("IDPRETENSION");
-				 ScsEJGAdm ejgdm = new ScsEJGAdm(this.usrbean);
-				
-				if(Idpretension!=null && !Idpretension.trim().equalsIgnoreCase("")){			
-						Vector vpretenciones=ejgdm.getPretension(Idpretension, idInstitucion);
-						for (int s = 0; s < vpretenciones.size(); s++) {
-							Hashtable registropretenciones = (Hashtable) vpretenciones.get(s);
-							String procedimiento = (String)registropretenciones.get("PRETENSION");
-							if(procedimiento!=null && !procedimiento.trim().equalsIgnoreCase(""))
-								registro.put("PROCEDIMIENTO_DESIGNA", procedimiento);
-							else
-								UtilidadesHash.set(registro, "PROCEDIMIENTO_DESIGNA", "");
-						}							
-							
-				}else	UtilidadesHash.set(registro, "PROCEDIMIENTO_DESIGNA", "");
-				
-								
 				helperInformes.completarHashSalida(registro,helperInformes.getJuzgadoSalida(idInstJuzgado,idJuzgado,""));
 				
 				String lugar = (String)registro.get("LUGAR");
@@ -2037,8 +2009,7 @@ public class ScsDesignaAdm extends MasterBeanAdministrador {
 				"              '', " +
 				"              Pkg_Siga_Fecha_En_Letra.f_Siga_Fechacompletaenletra(Letant.Fecharenunciasolicita, " +
 				"                                                                  'm', " +
-				"                                                                  1)) Fecha_Solicitudrenuncia_Letra, " +
-				" Des.Idpretension AS Idpretension "+
+				"                                                                  1)) Fecha_Solicitudrenuncia_Letra " +
 				"  From Scs_Designa         Des, " +
 				"       Scs_Designasletrado Let, " +
 				"       Cen_Persona         p, " +
