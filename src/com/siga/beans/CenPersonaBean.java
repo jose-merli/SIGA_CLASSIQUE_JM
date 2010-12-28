1261cefff114f64a2b2c76e55468002e3b1859f0
Adrian Ayala Gomez <adrian.ayala@redabogacia.org>
2010-12-28 10:24:19 +0000

    Limpieza

--- src/com/siga/beans/CenPersonaBean.java
+++ src/com/siga/beans/CenPersonaBean.java
@@ -1,9 +1,3 @@
-/*
- * Created on 14-oct-2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package com.siga.beans;
 
 import com.siga.Utilidades.AjaxXMLBuilderAnnotation;
@@ -11,101 +5,100 @@ import com.siga.Utilidades.AjaxXMLBuilderNameAnnotation;
 import com.siga.Utilidades.AjaxXMLBuilderValueAnnotation;
 
 /**
- * @author daniel.campos
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * Created on 14-oct-2004
  */
 @AjaxXMLBuilderAnnotation
-public class CenPersonaBean extends MasterBean {
-
-
-	/* Variables */
+public class CenPersonaBean extends MasterBean
+{
+	// Atributos
 	private Long idPersona;
-	private Integer idTipoIdentificacion, idEstadoCivil;
-	private String nombre, apellido1, apellido2, nifcif, naturalDe,sexo;  
-	private String fechaNacimiento,fallecido;
-	private boolean existeDatos;
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private String nifcif;
+	private Integer idTipoIdentificacion;
+	private Integer idEstadoCivil;
+	private String fechaNacimiento;
+	private String naturalDe;
+	private String sexo;  
+	private String fallecido;
 	
-	/* Nombre tabla */
+	private boolean existeDatos;
+	CenColegiadoBean colegiado;
+	
+	/** Nombre tabla */
 	static public String T_NOMBRETABLA = "CEN_PERSONA";
 
-	/* Nombre campos de la tabla */
+	// Nombre campos de la tabla
 	static public final String C_IDPERSONA 				= "IDPERSONA";
 	static public final String C_NOMBRE 				= "NOMBRE";
 	static public final String C_APELLIDOS1 			= "APELLIDOS1";
 	static public final String C_APELLIDOS2 			= "APELLIDOS2";
 	static public final String C_NIFCIF 				= "NIFCIF";
-	static public final String C_FECHANACIMIENTO 		= "FECHANACIMIENTO";
-	static public final String C_IDESTADOCIVIL 			= "IDESTADOCIVIL";
 	static public final String C_IDTIPOIDENTIFICACION 	= "IDTIPOIDENTIFICACION";
+	static public final String C_IDESTADOCIVIL 			= "IDESTADOCIVIL";
+	static public final String C_FECHANACIMIENTO 		= "FECHANACIMIENTO";
 	static public final String C_NATURALDE 				= "NATURALDE";
 	static public final String C_SEXO				    = "SEXO";
 	static public final String C_FALLECIDO				= "FALLECIDO";
 	
-	// Metodos SET
-	public void setIdPersona (Long id) 			{ this.idPersona = id; }
-	public void setIdEstadoCivil (Integer id) 		{ this.idEstadoCivil = id; }
-	public void setIdTipoIdentificacion (Integer id){ this.idTipoIdentificacion = id; }
-	public void setNombre (String n) 				{ this.nombre = n; }
-	public void setApellido1 (String a) 			{ this.apellido1 = a; }
-	public void setApellido2 (String a) 			{ this.apellido2 = a; }
-	public void setNIFCIF (String nif)				{ this.nifcif = nif; }
-	public void setFechaNacimiento (String f) 		{ this.fechaNacimiento = f; }
-	public void setNaturalDe (String s) 			{ this.naturalDe = s; }
-	public void setFallecido (String fallecido) 			{ this.fallecido = fallecido; }
 	
-	public void setExisteDatos(boolean existeDatos) {this.existeDatos = existeDatos;}
-	// Metodos GET
-	@AjaxXMLBuilderValueAnnotation(isCData=false)
-	public Long getIdPersona 			()	{ return this.idPersona; }
-	public Integer getIdEstadoCivil 		()	{ return this.idEstadoCivil; }
-	public Integer getIdTipoIdentificacion	() 	{ return this.idTipoIdentificacion; }
-	@AjaxXMLBuilderNameAnnotation
-	public String getNombre 			()	{ return this.nombre; }
-	public String getApellido1 			()	{ return this.apellido1; }
-	public String getApellido2 			() 	{ return this.apellido2; }
-	public String getNIFCIF 			() 	{ return this.nifcif; }
-	public String getNaturalDe 			() 	{ return this.naturalDe; }
-	public String getFechaNacimiento 		() 	{ return this.fechaNacimiento; }
-	public String getFallecido 			() 	{ return this.fallecido; }
-	public boolean isExisteDatos() { return existeDatos; }
 	/**
-	 * @return Returns the sexo.
+	 * Constructor por defecto
 	 */
-	public String getSexo() {
-		return sexo;
-	}
-	/**
-	 * @param sexo The sexo to set.
-	 */
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
-	CenColegiadoBean colegiado;
-
-	public CenColegiadoBean getColegiado() {
-		return colegiado;
-	}
-	public void setColegiado(CenColegiadoBean colegiado) {
-		this.colegiado = colegiado;
-	}
 	public CenPersonaBean() {
 		super();
 	}
-	public CenPersonaBean(Long idPersona, String nombre, String apellido1,
-			String apellido2,String nColegiado) {
+	/**
+	 * Constructor
+	 */
+	public CenPersonaBean(Long idPersona, String nombre, String apellido1, String apellido2, String nColegiado)
+	{
 		super();
 		this.idPersona = idPersona;
 		this.nombre = nombre;
 		this.apellido1 = apellido1;
 		this.apellido2 = apellido2;
+		
 		CenColegiadoBean colegiado = new CenColegiadoBean();
 		colegiado.setNColegiado(nColegiado);
 		this.colegiado = colegiado;
-		
-		
 	}
 	
+	
+	// SETTERS
+	public void setIdPersona			(Long valor)	{this.idPersona = valor;}
+	public void setNombre				(String valor) 	{this.nombre = valor;}
+	public void setApellido1			(String valor) 	{this.apellido1 = valor;}
+	public void setApellido2			(String valor) 	{this.apellido2 = valor;}
+	public void setNIFCIF				(String valor)	{this.nifcif = valor;}
+	public void setIdTipoIdentificacion	(Integer valor)	{this.idTipoIdentificacion = valor;}
+	public void setIdEstadoCivil		(Integer valor) {this.idEstadoCivil = valor;}
+	public void setFechaNacimiento		(String valor) 	{this.fechaNacimiento = valor;}
+	public void setNaturalDe			(String valor) 	{this.naturalDe = valor;}
+	public void setSexo					(String valor)	{this.sexo = valor;}
+	public void setFallecido			(String valor)	{this.fallecido = valor;}
+	
+	public void setExisteDatos(boolean existeDatos) {this.existeDatos = existeDatos;}
+	public void setColegiado(CenColegiadoBean colegiado) {this.colegiado = colegiado;}
+	
+	
+	// GETTERS
+	@AjaxXMLBuilderValueAnnotation(isCData=false)
+	public Long getIdPersona 				() {return this.idPersona;}
+	public Integer getIdEstadoCivil 		() {return this.idEstadoCivil;}
+	public Integer getIdTipoIdentificacion	() {return this.idTipoIdentificacion;}
+	@AjaxXMLBuilderNameAnnotation
+	public String getNombre 				() {return this.nombre;}
+	public String getApellido1 				() {return this.apellido1;}
+	public String getApellido2 				() {return this.apellido2;}
+	public String getNIFCIF 				() {return this.nifcif;}
+	public String getFechaNacimiento 		() {return this.fechaNacimiento;}
+	public String getNaturalDe 				() {return this.naturalDe;}
+	public String getSexo					() {return this.sexo;}
+	public String getFallecido 				() {return this.fallecido;}
+	
+	public boolean isExisteDatos() {return existeDatos;}
+	public CenColegiadoBean getColegiado() {return colegiado;}
 	
 }
