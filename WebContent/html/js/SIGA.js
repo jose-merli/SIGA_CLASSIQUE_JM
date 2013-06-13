7d7b4276bf3cd454a6edb3a692c1fe12b160550a
T F 2 <papelera@redabogacia.org>
2013-06-13 14:14:41 +0000

    BNS: AÑADIDOS PLUGINS Y PARCHE PARA CONSOLE EN IE

--- WebContent/html/js/SIGA.js
+++ WebContent/html/js/SIGA.js
@@ -1,3 +1,4 @@
+var console = window.console || { log: function() {} };
 var jqueryFileUri = "/SIGA/html/js/jquery.js";
 if (typeof jQuery == "undefined"){
 	// Tratamos de usar jQuery de window.top
@@ -60,7 +61,21 @@ function jQueryLoaded(){
 	// Inicialización de jquery para el contexto actual
 	window.top.jQueryContext(window);
 	
-	// *** BEGIN JQUERY PLUGINS *** //
+	// *** BEGIN PLUGINS *** //
+	
+	// EXISTS FUNCTION
+	jQuery.fn.exists = function(){return this.length>0;};
+	
+	//JSON FOR COMPATIBILITY MODE ON IE
+	//http://cdnjs.cloudflare.com/ajax/libs/json2/20110223/json2.min.js
+	var JSON;JSON||(JSON={});
+	(function(){function k(a){return a<10?"0"+a:a}function o(a){p.lastIndex=0;return p.test(a)?'"'+a.replace(p,function(a){var c=r[a];return typeof c==="string"?c:"\\u"+("0000"+a.charCodeAt(0).toString(16)).slice(-4)})+'"':'"'+a+'"'}function l(a,j){var c,d,h,m,g=e,f,b=j[a];b&&typeof b==="object"&&typeof b.toJSON==="function"&&(b=b.toJSON(a));typeof i==="function"&&(b=i.call(j,a,b));switch(typeof b){case "string":return o(b);case "number":return isFinite(b)?String(b):"null";case "boolean":case "null":return String(b);case "object":if(!b)return"null";
+	e+=n;f=[];if(Object.prototype.toString.apply(b)==="[object Array]"){m=b.length;for(c=0;c<m;c+=1)f[c]=l(c,b)||"null";h=f.length===0?"[]":e?"[\n"+e+f.join(",\n"+e)+"\n"+g+"]":"["+f.join(",")+"]";e=g;return h}if(i&&typeof i==="object"){m=i.length;for(c=0;c<m;c+=1)typeof i[c]==="string"&&(d=i[c],(h=l(d,b))&&f.push(o(d)+(e?": ":":")+h))}else for(d in b)Object.prototype.hasOwnProperty.call(b,d)&&(h=l(d,b))&&f.push(o(d)+(e?": ":":")+h);h=f.length===0?"{}":e?"{\n"+e+f.join(",\n"+e)+"\n"+g+"}":"{"+f.join(",")+
+	"}";e=g;return h}}if(typeof Date.prototype.toJSON!=="function")Date.prototype.toJSON=function(){return isFinite(this.valueOf())?this.getUTCFullYear()+"-"+k(this.getUTCMonth()+1)+"-"+k(this.getUTCDate())+"T"+k(this.getUTCHours())+":"+k(this.getUTCMinutes())+":"+k(this.getUTCSeconds())+"Z":null},String.prototype.toJSON=Number.prototype.toJSON=Boolean.prototype.toJSON=function(){return this.valueOf()};var q=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
+	p=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,e,n,r={"\u0008":"\\b","\t":"\\t","\n":"\\n","\u000c":"\\f","\r":"\\r",'"':'\\"',"\\":"\\\\"},i;if(typeof JSON.stringify!=="function")JSON.stringify=function(a,j,c){var d;n=e="";if(typeof c==="number")for(d=0;d<c;d+=1)n+=" ";else typeof c==="string"&&(n=c);if((i=j)&&typeof j!=="function"&&(typeof j!=="object"||typeof j.length!=="number"))throw Error("JSON.stringify");return l("",
+	{"":a})};if(typeof JSON.parse!=="function")JSON.parse=function(a,e){function c(a,d){var g,f,b=a[d];if(b&&typeof b==="object")for(g in b)Object.prototype.hasOwnProperty.call(b,g)&&(f=c(b,g),f!==void 0?b[g]=f:delete b[g]);return e.call(a,d,b)}var d,a=String(a);q.lastIndex=0;q.test(a)&&(a=a.replace(q,function(a){return"\\u"+("0000"+a.charCodeAt(0).toString(16)).slice(-4)}));if(/^[\],:{}\s]*$/.test(a.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,"@").replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,
+	"]").replace(/(?:^|:|,)(?:\s*\[)+/g,"")))return d=eval("("+a+")"),typeof e==="function"?c({"":d},""):d;throw new SyntaxError("JSON.parse");}})();
+	
 	/**
 	*	jQuery.noticeAdd() and jQuery.noticeRemove()
 	*	These functions create and remove growl-like notices
@@ -87,7 +102,7 @@ function jQueryLoaded(){
 	*	
 	*	@author 	Tim Benniks <tim@timbenniks.com>
 	* 	@copyright  2009 timbenniks.com
-	*	@version    $Id: SIGA.js,v 1.54 2013-05-31 12:46:04 tf2 Exp $
+	*	@version    $Id: SIGA.js,v 1.55 2013-06-13 14:14:41 tf2 Exp $
 	**/
 	(function(jQuery)
 	{
@@ -159,7 +174,151 @@ function jQueryLoaded(){
 		}
 	});
 	
-	//*** END JQUERY PLUGINS *** //
+	/**
+	 * jQuery Watch Plugin
+	 *
+	 * @author Darcy Clarke
+	 * @version 2.0
+	 *
+	 * Copyright (c) 2012 Darcy Clarke
+	 * Dual licensed under the MIT and GPL licenses.
+	 *
+	 * ADDS: 
+	 *
+	 * - $.watch()
+	 *  
+	 * USES:
+	 *
+	 * - DOMAttrModified event
+	 * 
+	 * FALLBACKS:
+	 * 
+	 * - propertychange event
+	 * - setTimeout() with delay 
+	 *
+	 * EXAMPLE:
+	 * 
+	 * $('div').watch('width height', function(){
+	 *      console.log(this.style.width, this.style.height);
+	 * });
+	 *
+	 * $('div').animate({width:'100px',height:'200px'}, 500);
+	 *
+	 */
+
+	(function($){
+	    
+	    $.extend($.fn, {         
+	        
+	        /**
+	         * Watch Method
+	         * 
+	         * @param {String} the name of the properties to watch
+	         * @param {Object} options to overide defaults (only 'throttle' right now)
+	         * @param {Function} callback function to be executed when attributes change
+	         *
+	         * @return {jQuery Object} returns the jQuery object for chainability
+	         */   
+	        watch : function(props, options, callback){
+
+	            // Dummmy element
+	            var element = document.createElement('div');
+
+	            /**
+	             * Checks Support for Event
+	             * 
+	             * @param {String} the name of the event
+	             * @param {Element Object} the element to test support against
+	             *
+	             * @return {Boolean} returns result of test (true/false)
+	             */
+	            var isEventSupported = function(eventName, el) {
+	                eventName = 'on' + eventName;
+	                var supported = (eventName in el);
+	                if(!supported){
+	                    el.setAttribute(eventName, 'return;');
+	                    supported = typeof el[eventName] == 'function';
+	                }
+	                return supported;
+	            };
+
+	            // Type check options
+	            if(typeof(options) == 'function'){
+	                callback = options;
+	                options = {};
+	            }
+
+	            // Type check callback
+	            if(typeof(callback) != 'function')
+	                callback = function(){};
+
+	            // Map options over defaults
+	            options = $.extend({}, { throttle : 10 }, options);
+
+	            /**
+	             * Checks if properties have changed
+	             * 
+	             * @param {Element Object} the element to watch
+	             *
+	             */
+	            var check = function(el) {
+	                var data = el.data(),
+	                    changed = false,
+	                    temp;
+
+	                // Loop through properties
+	                for(var i=0;i < data.props.length; i++){
+	                    temp = el.css(data.props[i]);
+	                    if(data.vals[i] != temp){
+	                        data.vals[i] = temp;
+	                        changed = true;
+	                        break;
+	                    }
+	                }
+	                
+	                // Run callback if property has changed
+	                if(changed && data.cb)
+	                    data.cb.call(el, data);
+	            };
+
+	            return this.each(function(){
+	                var el = $(this),
+	                    cb = function(){ check.call(this, el) },
+	                    data = { props:props.split(','), cb:callback, vals: [] };
+	                $.each(data.props, function(i){ data.vals[i] = el.css(data.props[i]); });
+	                el.data(data);
+	                if(isEventSupported('DOMAttrModified', element)){
+	                    el.on('DOMAttrModified', callback);
+	                } else if(isEventSupported('propertychange', element)){
+	                    el.on('propertychange', callback);
+	                } else {
+	                    setInterval(cb, options.throttle);
+	                }
+	            });
+	        }
+	    });
+	})(jQuery);
+	
+	//serializeObject
+	
+	jQuery.fn.serializeObject = function()
+	{
+	    var o = {};
+	    var a = this.serializeArray();
+	    jQuery.each(a, function() {
+	        if (o[this.name] !== undefined) {
+	            if (!o[this.name].push) {
+	                o[this.name] = [o[this.name]];
+	            }
+	            o[this.name].push(this.value || '');
+	        } else {
+	            o[this.name] = this.value || '';
+	        }
+	    });
+	    return o;
+	};
+	
+	//*** END PLUGINS *** //
 	
 	/*  Hack for allowing correct typing in modal dialogs in safari. */
 	try {
