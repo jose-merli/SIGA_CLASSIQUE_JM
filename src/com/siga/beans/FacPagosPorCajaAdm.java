e9c9241e6623811d45b5677f81592d972b968356
raquel <papelera@redabogacia.org>
2013-11-20 12:35:48 +0000

    Ordenación pestaña Pagos INC_11476_SIGA

--- src/com/siga/beans/FacPagosPorCajaAdm.java
+++ src/com/siga/beans/FacPagosPorCajaAdm.java
@@ -375,7 +375,7 @@ public class FacPagosPorCajaAdm extends MasterBeanAdministrador {
 							   " where " + FacEstadoFacturaBean.C_IDESTADO + " = 5) " +
 					 		 " end as estado, " +			
 							 "renegociacion." + FacRenegociacionBean.C_FECHARENEGOCIACION   + " AS FECHA, "   + 
-							 "renegociacion." + FacRenegociacionBean.C_FECHARENEGOCIACION   + " AS FECHA_ORDEN, "   + 					 		 
+							 "renegociacion." + FacRenegociacionBean.C_FECHAMODIFICACION   + " AS FECHA_ORDEN, "   + 					 		 
 				  			 "renegociacion." + FacRenegociacionBean.C_IMPORTE + " AS IMPORTE, " + 
 							 "'' AS DEVUELTA, '' AS tarjeta, " +
 							 " 0 AS idabono_idcuenta, '' AS numeroabono, 0 AS idpago, " +
@@ -432,7 +432,7 @@ public class FacPagosPorCajaAdm extends MasterBeanAdministrador {
 
 			String consulta = "SELECT idtabla, TABLA, ESTADO, FECHA, FECHA_ORDEN, IMPORTE, DEVUELTA, TARJETA, IDABONO_IDCUENTA, NUMEROABONO, IDPAGO, NOMBREBANCO FROM ( " + 
 							   consulta1 + " UNION " + consulta10 + " UNION " + consulta2 + " UNION " + consulta3 + " UNION " + consulta4 + " UNION " + consulta5 + " UNION " + consulta6 + " UNION " + consulta7 + 
-							   " ) ORDER BY idtabla ASC, FECHA_ORDEN ASC, FECHA ASC, IDPAGO ASC"; 
+							   " ) ORDER BY idtabla ASC, FECHA ASC, FECHA_ORDEN ASC, IDPAGO ASC"; 
 
 			RowsContainer rc = new RowsContainer(); 
 			if (rc.query(consulta)) {
