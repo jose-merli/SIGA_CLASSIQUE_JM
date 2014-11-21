6f4a1d67d3bf85a26c11980e1f9b114915634125
Adrian Ayala Gomez <adrian.ayala@redabogacia.org>
2014-11-21 13:02:59 +0000

    118_001: R1408_0006: Notas Adrian (18-11-2014): Marcha atras del cambio de los timers, ya que resulta imposible controlar la generacion encadenada de timers.

--- src/com/siga/servlets/SIGASvlProcesoAutomaticoFacturacion.java
+++ src/com/siga/servlets/SIGASvlProcesoAutomaticoFacturacion.java
@@ -1,103 +1,119 @@
 package com.siga.servlets;
 
 import java.net.URL;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.Date;
 
+import javax.management.InstanceNotFoundException;
+import javax.management.Notification;
+import javax.management.NotificationListener;
 import javax.servlet.ServletException;
 
-import com.atos.utils.ClsLogging;
 import org.redabogacia.sigaservices.app.util.ReadProperties;
 import org.redabogacia.sigaservices.app.util.SIGAReferences;
 
+import weblogic.management.timer.Timer;
+
+import com.atos.utils.ClsLogging;
+
 /**
- * Servlet del proceso automatico de facturacion. Utiliza a su vez al servlet SIGASvlProcesoFacturacion
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>SchlumbergerSema: </p>
+ * @SchlumbergerSema
+ * @version 1.0
  */
-public class SIGASvlProcesoAutomaticoFacturacion extends SIGAServletAdapter
-{
-	private static final long serialVersionUID = 1L;
-	private static String sNombreProceso = "ProcesoAutomaticoFacturacion";
-	
-	/**
-	 * Inicializacion del servidor: se ejecuta cuando se inicia/arranca el servidor
-	 */
-	public void init() throws ServletException
-	{
-		super.init();
+public class SIGASvlProcesoAutomaticoFacturacion extends SIGAServletAdapter implements NotificationListener {
+    private Timer timer;
+    private Integer idNotificacion;
+    private long lIntervalo = 1;
+    private String sNombreProceso = "ProcesoAutomaticoFacturacion";
+    private String urlSiga = "";
+    
+  public void init() throws ServletException {
+	  super.init();
 
-		ClsLogging.writeFileLogWithoutSession("<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>", 3);
-		ClsLogging.writeFileLogWithoutSession(" Arrancando Notificaciones JMX.", 3);
+    //ClsLogging.writeFileLogWithoutSession("", 3);
+  	//ClsLogging.writeFileLogWithoutSession("", 3);
+    ClsLogging.writeFileLogWithoutSession("<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>", 3);
+    ClsLogging.writeFileLogWithoutSession(" Arrancando Notificaciones JMX.", 3);
 
-		iniciarTimer();
-		
-		ClsLogging.writeFileLogWithoutSession(" Notificaciones JMX arrancadas.", 3);
-		ClsLogging.writeFileLogWithoutSession("<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>", 3);
-	}
+    
+    ReadProperties properties= new ReadProperties(SIGAReferences.RESOURCE_FILES.SIGA);
+    String sIntervaloAux = properties.returnProperty("facturacion.programacionAutomatica.tiempo.ciclo");
+    String sIntervalo = sIntervaloAux;
 
-	/**
-	 * Reinicia el timer para que se vuelva a ejecutar el proceso automatico de facturacion pasado un tiempo.
-	 * El tiempo se define por una propiedad interna, que es modificable por BD.
-	 * 
-	 * Se ejecuta al inicio del servidor y cada vez que termina cada pasada del proceso (SIGASvlProcesoFacturacion)
-	 * @throws ServletException
-	 */
-	public void iniciarTimer() throws ServletException
-	{
-		ReadProperties properties = new ReadProperties(SIGAReferences.RESOURCE_FILES.SIGA);
-		String sIntervalo = properties.returnProperty("facturacion.programacionAutomatica.tiempo.ciclo");
+    urlSiga = properties.returnProperty("general.urlSIGA");
+    
+    if (sIntervalo==null || sIntervalo.trim().equals("")) {
+        sIntervalo="0";
+    }
 
-		if (sIntervalo == null || sIntervalo.trim().equals("") || sIntervalo.trim().equals("0")) {
-			ClsLogging.writeFileLogWithoutSession("    - Notificación \"" + sNombreProceso + "\" no arrancada.", 3);
-			ClsLogging.writeFileLogWithoutSession("    - Intervalo de ejecución: Erróneo (" + sIntervalo + ").", 3);
+    if (sIntervalo.equals("0")) {
+        ClsLogging.writeFileLogWithoutSession("    - Notificación \"" + sNombreProceso + "\" no arrancada.", 3);
+        ClsLogging.writeFileLogWithoutSession("    - Intervalo de ejecución: Erróneo (" + sIntervaloAux + ").", 3);
+        
+    } else {
+        lIntervalo = Long.parseLong(sIntervalo)*60*1000;
 
-		} else {
-			long lIntervalo = Long.parseLong(sIntervalo) * 60 * 1000;
-			Timer timer = new Timer();
-			timer.schedule(tarea, lIntervalo);
+        timer = new Timer();
+        timer.addNotificationListener(this, null, sNombreProceso);
 
-			ClsLogging.writeFileLogWithoutSession("    - Notificación \"" + sNombreProceso + "\" arrancada.", 3);
-			ClsLogging.writeFileLogWithoutSession("    - Intervalo de ejecución: " + sIntervalo + " minuto(s).", 3);
+        Date timerTriggerAt = new Date((new Date()).getTime() + 60000L);
+        idNotificacion = timer.addNotification(sNombreProceso, sNombreProceso, this, timerTriggerAt, lIntervalo);
+
+        timer.start();
+
+        ClsLogging.writeFileLogWithoutSession("    - Notificación \"" + sNombreProceso + "\" arrancada.", 3);
+        ClsLogging.writeFileLogWithoutSession("    - Intervalo de ejecución: " + sIntervalo + " minuto(s).", 3);
+    }
+
+    ClsLogging.writeFileLogWithoutSession(" Notificaciones JMX arrancadas.", 3);
+    ClsLogging.writeFileLogWithoutSession("<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>", 3);
+    //ClsLogging.writeFileLogWithoutSession("", 3);
+    //ClsLogging.writeFileLogWithoutSession("", 3);  	
+  }
+  
+  /* (non-Javadoc)
+ * @see javax.servlet.GenericServlet#destroy()
+ */
+ public void destroy() {
+ 	//ClsLogging.writeFileLogWithoutSession("", 3);
+ 	//ClsLogging.writeFileLogWithoutSession("", 3);
+    ClsLogging.writeFileLogWithoutSession("<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>", 3);
+    ClsLogging.writeFileLogWithoutSession(" Destruyendo notificaciones JMX.", 3);
+
+    try {
+		if (timer!=null) {
+			if (timer.isActive())
+				timer.stop();
+			timer.removeNotification(idNotificacion);
 		}
+
+        ClsLogging.writeFileLogWithoutSession("    - Notificación \"" + sNombreProceso + "\" parada.", 3);
+        ClsLogging.writeFileLogWithoutSession(" Notificaciones JMX destruídas.", 3);
+        ClsLogging.writeFileLogWithoutSession("<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>", 3);
+        //ClsLogging.writeFileLogWithoutSession("", 3);
+        //ClsLogging.writeFileLogWithoutSession("", 3);
+        
+	} catch (InstanceNotFoundException e) {
+        e.printStackTrace();
+    }
+ }
+
+ public void handleNotification(Notification notif, Object handback) {
+    ClsLogging.writeFileLogWithoutSession(" - INVOCANDO...  >>>  Ejecutando Notificación: \"" + sNombreProceso + "\".", 3);
+
+	try {
+	    // invocamos al servlet
+		URL url = new URL(urlSiga+ "SIGASvlProcesoFacturacion.svrl");
+	    Object ret = url.getContent();
+	    ClsLogging.writeFileLogWithoutSession(" - OK.  >>>  Ejecutando Notificación: \"" + sNombreProceso + "\".", 3);
+	    //ClsLogging.writeFileLogWithoutSession(" - OK. >>>  Ejecutando Notificación: \"" + urlSiga+ "SIGASvlProcesoFacturacion.svrl" + "\"", 3);
+	    
+	} catch(Exception e) {
+	    ClsLogging.writeFileLogWithoutSession(" - Notificación \"" + sNombreProceso + "\" ejecutada ERROR. ", 3);
+	    e.printStackTrace();
 	}
-
-	private TimerTask tarea = new TimerTask () {
-		/**
-		 * Este metodo se ejecuta cuando el timer termina y lanza este proceso de nuevo.
-		 * Solo sirve para dejar registro en LOG.
-		 * 
-		 * Nota: no se puede utilizar para reiniciar el timer, ya que no sabemos cuando termina el proceso
-		 */
-		public void run() {
-			ClsLogging.writeFileLogWithoutSession(" - INVOCANDO...  >>>  Ejecutando Notificación: \"" + sNombreProceso + ".", 3);
-	
-			try {
-				// invocando al servlet
-				ReadProperties properties = new ReadProperties(SIGAReferences.RESOURCE_FILES.SIGA);
-				String urlSiga = properties.returnProperty("general.urlSIGA");
-				URL url = new URL(urlSiga + "SIGASvlProcesoFacturacion.svrl");
-				url.getContent();
-				
-				// registrando el exito en la invocacion
-				ClsLogging.writeFileLogWithoutSession(" - OK.  >>>  Ejecutando Notificación: \"" + sNombreProceso + "\".", 3);
-	
-			} catch (Exception e) {
-				// registrando el error en la invocacion
-				ClsLogging.writeFileLogWithoutSession(" - Notificación \"" + sNombreProceso + "\" ejecutada ERROR. ", 3);
-				e.printStackTrace();
-			}
-		}
-	};
-
-	/**
-	 * Finalizacion del servlet: se ejecuta cuando se termina/apaga el servidor
-	 */
-	public void destroy()
-	{
-		ClsLogging.writeFileLogWithoutSession("<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>", 3);
-		ClsLogging.writeFileLogWithoutSession(" Destruyendo notificaciones JMX.", 3);
-
-		ClsLogging.writeFileLogWithoutSession("    - Notificación \"" + sNombreProceso + "\" parada.", 3);
-		ClsLogging.writeFileLogWithoutSession(" Notificaciones JMX destruídas.", 3);
-		ClsLogging.writeFileLogWithoutSession("<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>", 3);
-	}
+ } 
 }
