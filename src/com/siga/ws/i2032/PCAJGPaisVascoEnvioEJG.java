f5e16008d4532090bdda5398e383bcbd15b467e3
Angel Coello Puebla <angel.coello@redabogacia.org>
2014-12-15 11:36:09 +0000

    Añadir estado validando a los ejgs

--- src/com/siga/ws/i2032/PCAJGPaisVascoEnvioEJG.java
+++ src/com/siga/ws/i2032/PCAJGPaisVascoEnvioEJG.java
@@ -2,9 +2,13 @@ package com.siga.ws.i2032;
 
 import org.apache.log4j.Logger;
 import org.redabogacia.sigaservices.app.AppConstants;
-import org.redabogacia.sigaservices.app.autogen.model.CajgRemesaKey;
+import org.redabogacia.sigaservices.app.AppConstants.GEN_RECURSOS;
+import org.redabogacia.sigaservices.app.autogen.model.CajgRemesa;
 import org.redabogacia.sigaservices.app.autogen.model.EcomCola;
+import org.redabogacia.sigaservices.app.services.caj.CajgRemesaService;
 import org.redabogacia.sigaservices.app.services.ecom.EcomColaService;
+
+import com.siga.Utilidades.UtilidadesString;
 
 import es.satec.businessManager.BusinessManager;
 
@@ -15,7 +19,9 @@ public class PCAJGPaisVascoEnvioEJG extends PCAJGPaisVascoComun {
 	@Override
 	public void execute() throws Exception {	
 		
+		CajgRemesaService cajgRemesaService = (CajgRemesaService) BusinessManager.getInstance().getService(CajgRemesaService.class);
 		EcomColaService ecomColaService = (EcomColaService) BusinessManager.getInstance().getService(EcomColaService.class);
+		
 		EcomCola ecomCola = new EcomCola();
 		ecomCola.setIdinstitucion((short)getIdInstitucion());
 		if (isSimular()) {
@@ -24,9 +30,13 @@ public class PCAJGPaisVascoEnvioEJG extends PCAJGPaisVascoComun {
 			ecomCola.setIdoperacion(AppConstants.OPERACION.GV_ENVIO_EXPEDIENTES.getId());
 		}
 		log.debug("Insertando un nuevo registro para la validación de datos de una remesa de envío.");
-		CajgRemesaKey cajgRemesaKey = new CajgRemesaKey();
+		CajgRemesa cajgRemesaKey = new CajgRemesa();
 		cajgRemesaKey.setIdinstitucion((short)getIdInstitucion());
 		cajgRemesaKey.setIdremesa((long)getIdRemesa());
+		
+		cajgRemesaService.deleteCajgRespuestasRemesa(cajgRemesaKey);
+		cajgRemesaService.insertaEstadoValidandoRemesa(cajgRemesaKey, UtilidadesString.getMensajeIdioma(getUsrBean(), GEN_RECURSOS.scs_mensaje_validando.getValor()));
+		
 		ecomColaService.insertaColaRemesa(ecomCola, cajgRemesaKey);
 		
 	}
