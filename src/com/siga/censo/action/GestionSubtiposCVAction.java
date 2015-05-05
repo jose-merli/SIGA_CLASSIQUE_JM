b211b7e7a49fe81a9687ff6b53876f6467277b9f
Jorge Torres Acosta <jorge.torres@redabogacia.org>
2015-05-05 13:00:49 +0000

    QUITANDO WARNINGS

--- src/com/siga/censo/action/GestionSubtiposCVAction.java
+++ src/com/siga/censo/action/GestionSubtiposCVAction.java
@@ -1,9 +1,7 @@
 package com.siga.censo.action;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
