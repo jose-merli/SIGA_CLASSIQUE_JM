2c840530607359076aea8fc0c67af5d70fd79257
Carlos Ruano Martinez <carlos.ruano@redabogacia.org>
2015-06-10 06:49:46 +0000

    Parche

--- src/com/siga/servlets/SIGASvlProcesoGeneracionEnvio.java
+++ src/com/siga/servlets/SIGASvlProcesoGeneracionEnvio.java
@@ -10,65 +10,57 @@ import javax.servlet.http.HttpServletResponse;
 import org.redabogacia.sigaservices.app.AppConstants.PARAMETRO;
 
 import com.atos.utils.ClsLogging;
+import com.atos.utils.UsrBean;
+import com.siga.beans.GenParametrosAdm;
 import com.siga.envios.EnvioInformesGenericos;
-
 
 /**
  * 
  * @author jorgeta
- *
+ * 
  */
 public class SIGASvlProcesoGeneracionEnvio extends HttpServlet {
 
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private final String sNombreProceso = "ProcesoGeneracionEnvio";
 
-    
-// version de una sola llamada
+	// version de una sola llamada
 
+	public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
 
-    public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-    	doPost(request,response);
-    }
-    public void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-    	java.io.PrintWriter out = response.getWriter();
-   		try
-   		{
-   			out.println("INICIO PROCESO AUTOMATICO DE GENERACION DE ENVIOS EN BACKGROUND");
-			
-   			EnvioInformesGenericos envios = new EnvioInformesGenericos((String) request.getSession().getAttribute(PARAMETRO.LONGITUD_CODEJG.toString()));
-   			envios.procesarAutomaticamenteGeneracionEnvios();
-   			
-   	        response.setContentType("text/html");
-   	        out.println("FIN PROCESO AUTOMATICO DE GENERACION ENVIOS EN BACKGROUND");
-   	        out.close();
+	public void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+		java.io.PrintWriter out = response.getWriter();
+		try {
+			out.println("INICIO PROCESO AUTOMATICO DE GENERACION DE ENVIOS EN BACKGROUND");
+			EnvioInformesGenericos envios = new EnvioInformesGenericos("5");
+			envios.procesarAutomaticamenteGeneracionEnvios();
 
-   		}
+			response.setContentType("text/html");
+			out.println("FIN PROCESO AUTOMATICO DE GENERACION ENVIOS EN BACKGROUND");
+			out.close();
 
-   		catch(Exception e)
-   		{
-   			
-   	        response.setContentType("text/html");
-   	        
-   	        out.println("ERROR EN PROCESO AUTOMATICO DE GENERACION DE ENVIOS EN BACKGROUND");
-   			ClsLogging.writeFileLogWithoutSession(" - Notificación \"" + sNombreProceso + "\" ejecutada ERROR. : " + e.toString() , 3);
-   		    e.printStackTrace();
-   		}finally{
-   			if(out!=null){
-   				out.flush();
-   				out.close();
-   				out = null;
-   				
-   			}
-   			
-   			
-   		}
+		} catch (Exception e) {
 
-    }
+			response.setContentType("text/html");
+			out.println("ERROR EN PROCESO AUTOMATICO DE GENERACION DE ENVIOS EN BACKGROUND");
+			ClsLogging.writeFileLogWithoutSession(" - Notificación \"" + sNombreProceso + "\" ejecutada ERROR. : " + e.toString(), 3);
+			e.printStackTrace();
+
+		} finally {
+			if (out != null) {
+				out.flush();
+				out.close();
+				out = null;
+
+			}
+
+		}
+
+	}
 
 }
-
-
