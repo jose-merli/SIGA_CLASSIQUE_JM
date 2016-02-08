f7494d19675bfc78e132af7c8861d1935f49926c
Jorge Torres Acosta <jorge.torres@redabogacia.org>
2016-02-08 13:55:00 +0000

    R1508_0016 Se incluye el numero de procedimiento y el procedimiento de la designa a la actuacion en el alta masiva de actuaciones de designaciones en la ficha colegial. Para los Colegios EJIS se incluyo el año del procedimiento en la tabla SCS_DESIGNA por lo que se hace necesario incluir el campo ANIOPROCEDIMIENTO en la tabla SCS_ACTUACIONDESIGNA y traspasarlo tambien en la creacion de la actuacion

--- bd/Entregas/SIGA_123_001.sql
+++ bd/Entregas/SIGA_123_001.sql
@@ -2,5 +2,8 @@
 comment on column SCS_ASISTENCIA.IDORIGENASISTENCIA
   is '10 ICA SIGA 20 Colegiado SIGA  30 Volante expres móvil 40 Centralita de Guardias';
 
+  -- Add/modify columns 
+alter table SCS_ACTUACIONDESIGNA add ANIOPROCEDIMIENTO NUMBER(4);
 
+UPDATE scs_actuaciondesigna SET anioprocedimiento = anio where idinstitucion = 2014 and numeroprocedimiento is not null;
   
