1d878580db8048186480b21ec3226bbdfe136cdb
Jorge Paez Triviño <jorge.paez@redabogacia.org>
2016-02-09 10:33:24 +0000

    123_001 - R1507_0018: - Se ha habilitado un campo para observaciones con 1.000 caracteres de longitud máxima, que no es obligatorio introducir... Se ha añadido el campo CEN_HISTORICO.OBSERVACIONES [VARCHAR2(1000) NULLABLE] - A partir de la versión 123, se permite editar todas las auditorías. Pero sólo se puede modificar el campo observaciones, y sólo cuando no tenga un motivo indicado también el motivo... El resto de los campos se mostrarán en modo consulta y no podrán ser modificados.

--- bd/Entregas/SIGA_123_001.sql
+++ bd/Entregas/SIGA_123_001.sql
@@ -7,3 +7,10 @@ alter table SCS_ACTUACIONDESIGNA add ANIOPROCEDIMIENTO NUMBER(4);
 
 UPDATE scs_actuaciondesigna SET anioprocedimiento = anio where idinstitucion = 2014 and numeroprocedimiento is not null;
   
+ insert into GEN_RECURSOS (IDRECURSO, DESCRIPCION, ERROR, IDLENGUAJE, FECHAMODIFICACION, USUMODIFICACION, IDPROPIEDAD) values ('censo.consultaHistorico.literal.observaciones', 'Observaciones', 0, '1', sysdate, 0, '19');
+ insert into GEN_RECURSOS (IDRECURSO, DESCRIPCION, ERROR, IDLENGUAJE, FECHAMODIFICACION, USUMODIFICACION, IDPROPIEDAD) values ('censo.consultaHistorico.literal.observaciones', 'Observaciones#GL', 0, '4', sysdate, 0, '19');
+ insert into GEN_RECURSOS (IDRECURSO, DESCRIPCION, ERROR, IDLENGUAJE, FECHAMODIFICACION, USUMODIFICACION, IDPROPIEDAD) values ('censo.consultaHistorico.literal.observaciones', 'Observaciones#CA', 0, '2', sysdate, 0, '19');
+ insert into GEN_RECURSOS (IDRECURSO, DESCRIPCION, ERROR, IDLENGUAJE, FECHAMODIFICACION, USUMODIFICACION, IDPROPIEDAD) values ('censo.consultaHistorico.literal.observaciones', 'Observaciones#EU', 0, '3', sysdate, 0, '19');
+ 
+ALTER TABLE CEN_HISTORICO
+ADD OBSERVACIONES VARCHAR2(1000);
