57bfcfc661d407c99612234e6f95c79f1db409c9
Jose Mansilla <jose.mansilla@redabogacia.org>
2016-02-17 10:26:12 +0000

    122_008 - R1602_0074: Se modifica la forma de obtener el literal de la versión de SIGA desplegada.

--- src/com/siga/administracion/SIGAConstants.java
+++ src/com/siga/administracion/SIGAConstants.java
@@ -20,6 +20,7 @@ public interface SIGAConstants
 	public static final String PATH_LOGO="SIGA_LOGO";
 	public static final String STYLESHEET_PATH="directorios.carpeta.css";
 	public static final String ID_ANALYTICS="administracion.entorno.analytics";
+	public static final String PARAMETRO_ENTORNO = "ENTORNO";
 
 	/**
 	 * Para referenciar el path del logotipo de la institucion entre los atributos de sesión.
