000ac9ebe2b53bb59806c8a2a0ae39a7a9d48efc
Angel Coello Puebla <angel.coello@redabogacia.org>
2016-06-13 11:33:08 +0000

    petición de cambio R1606_0013, donde requiero la creación del nuevo campo "Circunstancias Excepcionales" para las Personas Justicia Gratuita.
    Y estaría bien aprovechar este nuevo cambio dels XSD para atender la antigua R1510_0003, que pide crear el nuevo campo Incapaz, también para las Personas Justicia Gratuita...

--- bd/Entregas/SIGA_123_XXX.sql
+++ bd/Entregas/SIGA_123_XXX.sql
@@ -9,3 +9,17 @@ Cambios en V_CENSO_LETRADOS, V_CENSO_COLEGIACIONES y V_CENSO_LETRADOS_OOJJ
 Cambio en V_CENSO_LETRADOS
 
 -- Los ejecutara Azucena fuera de horario laboral en los 4 entornos.
+
+alter table SCS_UNIDADFAMILIAREJG add INCAPACITADO number(1);
+alter table SCS_UNIDADFAMILIAREJG add CIRCUNSTANCIAS_EXCEPCIONALES number(1);
+
+ insert into GEN_RECURSOS (IDRECURSO, DESCRIPCION, ERROR, IDLENGUAJE, FECHAMODIFICACION, USUMODIFICACION, IDPROPIEDAD) values ('gratuita.personaJG.literal.incapacitado', 'Incapacitado', 0, '1', sysdate, 0, '19');
+ insert into GEN_RECURSOS (IDRECURSO, DESCRIPCION, ERROR, IDLENGUAJE, FECHAMODIFICACION, USUMODIFICACION, IDPROPIEDAD) values ('gratuita.personaJG.literal.incapacitado', 'Incapacitado#GL', 0, '4', sysdate, 0, '19');
+ insert into GEN_RECURSOS (IDRECURSO, DESCRIPCION, ERROR, IDLENGUAJE, FECHAMODIFICACION, USUMODIFICACION, IDPROPIEDAD) values ('gratuita.personaJG.literal.incapacitado', 'Incapacitado#CA', 0, '2', sysdate, 0, '19');
+ insert into GEN_RECURSOS (IDRECURSO, DESCRIPCION, ERROR, IDLENGUAJE, FECHAMODIFICACION, USUMODIFICACION, IDPROPIEDAD) values ('gratuita.personaJG.literal.incapacitado', 'Incapacitado#EU', 0, '3', sysdate, 0, '19');
+
+ insert into GEN_RECURSOS (IDRECURSO, DESCRIPCION, ERROR, IDLENGUAJE, FECHAMODIFICACION, USUMODIFICACION, IDPROPIEDAD) values ('gratuita.personaJG.literal.circunstanciasExcepcionales', 'Circunst. Excepc.', 0, '1', sysdate, 0, '19');
+ insert into GEN_RECURSOS (IDRECURSO, DESCRIPCION, ERROR, IDLENGUAJE, FECHAMODIFICACION, USUMODIFICACION, IDPROPIEDAD) values ('gratuita.personaJG.literal.circunstanciasExcepcionales', 'Circunst. Excepc.#GL', 0, '4', sysdate, 0, '19');
+ insert into GEN_RECURSOS (IDRECURSO, DESCRIPCION, ERROR, IDLENGUAJE, FECHAMODIFICACION, USUMODIFICACION, IDPROPIEDAD) values ('gratuita.personaJG.literal.circunstanciasExcepcionales', 'Circunst. Excepc.#CA', 0, '2', sysdate, 0, '19');
+ insert into GEN_RECURSOS (IDRECURSO, DESCRIPCION, ERROR, IDLENGUAJE, FECHAMODIFICACION, USUMODIFICACION, IDPROPIEDAD) values ('gratuita.personaJG.literal.circunstanciasExcepcionales', 'Circunst. Excepc.#EU', 0, '3', sysdate, 0, '19');
+
