0bb481592a38ef8a875220611a781f6942223ada
Adrian Ayala Gomez <adrian.ayala@redabogacia.org>
2016-06-29 08:21:43 +0000

    123_007 - R1606_0046 - Cambio de vista materializada V_CENSO_LETRADOS por una tabla con refresco.

--- bd/Entregas/SIGA_123_XXX.sql
+++ bd/Entregas/SIGA_123_XXX.sql
@@ -31,3 +31,43 @@ alter table SCS_UNIDADFAMILIAREJG add CIRCUNSTANCIAS_EXCEPCIONALES number(1);
 
 -- Ejecutado por Adrian en SIGA INT - 2016-06-20 08:25
 
+
+Drop Materialized View V_CENSO_LETRADOS;
+Drop Table V_CENSO_LETRADOS;
+
+Create Table T_CENSO_LETRADOS (
+  Id_Letrado Number(10),
+  Nombre Varchar2(100),
+  Apellido1 Varchar2(100),
+  Apellido2 Varchar2(100),
+  Num_doc Varchar2(20),
+  Idtipoidentificacion Number(2),
+  Sexo Varchar2(1),
+  Fechamodificacion Date,
+  
+  Dir_Profesional Varchar2(100),
+  Poblacion Varchar2(100),
+  Idpoblacion Varchar2(11),
+  Idprovincia Varchar2(2),
+  Provincia Varchar2(100),
+  Cod_Postal Varchar2(5),
+  Telefono Varchar2(20),
+  Fax Varchar2(20),
+  Movil Varchar2(20),
+  Mail Varchar2(100),
+  Idpais Varchar2(3),
+  Pais Varchar2(100)
+)
+Tablespace Ts_siga_censo;
+/
+
+Alter Table T_Censo_Letrados
+Add RegistroTemporal Varchar2(1) Default '1';
+
+create or replace view v_censo_letrados as
+select "ID_LETRADO","NOMBRE","APELLIDO1","APELLIDO2","NUM_DOC","IDTIPOIDENTIFICACION","SEXO","FECHAMODIFICACION","DIR_PROFESIONAL","POBLACION","IDPOBLACION","IDPROVINCIA","PROVINCIA","COD_POSTAL","TELEFONO","FAX","MOVIL","MAIL","IDPAIS","PAIS" from t_censo_letrados
+Where RegistroTemporal = 0;
+
+Creado Refresh_t_censo_Letrados.prc
+Modificado ECENSO.P_SYNC_ABOGADOS_REFRESH.prc
+
