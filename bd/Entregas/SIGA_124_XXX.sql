4054d4da2fddaecd4e7c2a4451dad49897de114a
Adrian Ayala Gomez <adrian.ayala@redabogacia.org>
2016-11-04 10:51:20 +0000

    124_003 - R1611_0002 - Cambio el proceso de copia al Consejo, para que si existe una dirección duplicada en el Consejo, se elimine. Añado un script para arreglar los casos existentes actualmente.

---
+++ bd/Entregas/SIGA_124_XXX.sql
@@ -0,0 +1,32 @@
+Pkg_Siga_Censo;
+
+Declare
+  Cursor c_direccionesduplicadas Is
+  Select dir.*
+    From Cen_Direcciones Dir, Cen_Direccion_Tipodireccion Tip
+   Where Dir.Idinstitucion = Tip.Idinstitucion
+     And Dir.Idpersona = Tip.Idpersona
+     And Dir.Iddireccion = Tip.Iddireccion
+     And Dir.Fechabaja Is Null
+     And Dir.Idinstitucion = 2000
+     And Tip.Idtipodireccion In (3, 9)
+     And Exists (Select 1
+            From Cen_Direcciones Dir2, Cen_Direccion_Tipodireccion Tip2
+           Where Dir2.Idinstitucion = Tip2.Idinstitucion
+             And Dir2.Idpersona = Tip2.Idpersona
+             And Dir2.Iddireccion = Tip2.Iddireccion
+             And Dir2.Fechabaja Is Null
+             
+             And Tip.Idinstitucion = Tip2.Idinstitucion
+             And Tip.Idpersona = Tip2.Idpersona
+             And Tip.Idtipodireccion = Tip2.Idtipodireccion
+             And Tip.Iddireccion > Tip2.Iddireccion);
+  p_Codretorno Varchar2(4000);
+  p_Datoserror Varchar2(4000);
+Begin
+  For r_dir In c_direccionesduplicadas Loop
+    Pkg_Siga_Censo.Actualizardatosletrado(r_dir.Idpersona, r_dir.Idinstitucion, 30, r_dir.Iddireccion, 0, p_Codretorno, p_Datoserror);
+  End Loop;
+End;
+/
+
