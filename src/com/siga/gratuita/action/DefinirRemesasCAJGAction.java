63445f2347199256b62b7f8ee76fa905c71ce42c
Adrian Ayala Gomez <adrian.ayala@redabogacia.org>
2017-05-23 08:34:33 +0000

    Angel subio algo de la Integracion de Cantabria y los sustitutos no sabemos a qué se refiere. Tampoco puso número de cambio asignado (para qué ponerlo, ¿verdad, Angel?), así que imposible seguir el rastro a una persona externa. Por todo esto, decido que hay que evitar el daño en SIGA y doy marcha atras.

--- src/com/siga/gratuita/action/DefinirRemesasCAJGAction.java
+++ src/com/siga/gratuita/action/DefinirRemesasCAJGAction.java
@@ -45,9 +45,9 @@ import org.redabogacia.sigaservices.app.AppConstants.PARAMETRO;
 import org.redabogacia.sigaservices.app.autogen.model.CajgRemesa;
 import org.redabogacia.sigaservices.app.autogen.model.EcomCola;
 import org.redabogacia.sigaservices.app.exceptions.BusinessException;
-import org.redabogacia.sigaservices.app.helper.AsignaVeredaHelper;
+//MARCHA ATRAS TEMPORAL import org.redabogacia.sigaservices.app.helper.AsignaVeredaHelper;
 import org.redabogacia.sigaservices.app.helper.SIGAServicesHelper;
-import org.redabogacia.sigaservices.app.helper.AsignaVeredaHelper.ASIGNA_VERSION;
+//MARCHA ATRAS TEMPORAL import org.redabogacia.sigaservices.app.helper.AsignaVeredaHelper.ASIGNA_VERSION;
 import org.redabogacia.sigaservices.app.services.caj.CajgRemesaService;
 import org.redabogacia.sigaservices.app.services.caj.PCAJGInsertaColaService;
 import org.redabogacia.sigaservices.app.services.ecom.EcomColaService;
@@ -1894,13 +1894,13 @@ public class DefinirRemesasCAJGAction extends MasterAction {
 		
 		boolean simular = VALUE_TRUE.equals(((DefinicionRemesas_CAJG_Form) formulario).getSimular());
 		
-		ASIGNA_VERSION versionAsignaVereda = null;
-		
+/* MARCHA ATRAS TEMPORAL
+ 		ASIGNA_VERSION versionAsignaVereda = null;		
 		if (CajgConfiguracion.TIPO_CAJG_WEBSERVICE_PAMPLONA == tipoCAJG) {
 			//si es asigna comprueba la version Si es la versión 2 va por ecom pq es la versión de vereda y si es 1 se deja lo antiguo como estaba en SIGA
 			versionAsignaVereda = AsignaVeredaHelper.getAsignaVersion(Short.valueOf(idInstitucion.toString()));
 		}
-		
+*/		
 		if (CajgConfiguracion.TIPO_CAJG_WEBSERVICE_PAISVASCO == tipoCAJG) {
 			PCAJGInsertaColaService pcajgInsertaColaService = (PCAJGInsertaColaService) getBusinessManager().getService(PCAJGInsertaColaService.class);
 			RESPUESTA_ENVIO_REMESA respuesta = null;
@@ -1938,7 +1938,8 @@ public class DefinirRemesasCAJGAction extends MasterAction {
 			mensaje = getMensajeRespuesta(respuesta, request, simular);
 		} else if (CajgConfiguracion.TIPO_CAJG_TXT_ALCALA == tipoCAJG) {
 			mensaje = validaRemesaTxt(mapping, formulario, request, response);
-			
+
+/* MARCHA ATRAS TEMPORAL
 		} else if (versionAsignaVereda != null && ASIGNA_VERSION.VERSION_2.equals(versionAsignaVereda)) {
 			PCAJGInsertaColaService pcajgInsertaColaService = (PCAJGInsertaColaService) getBusinessManager().getService(PCAJGInsertaColaService.class);
 			RESPUESTA_ENVIO_REMESA respuesta = null;
@@ -1951,6 +1952,7 @@ public class DefinirRemesasCAJGAction extends MasterAction {
 			}
 			
 			mensaje = getMensajeRespuesta(respuesta, request, simular);
+*/
 		} else {
 			ejecutaBackground(formulario, request, 0);	
 			if (simular) {
