35ca1cff18841b8c087335289e0182e04369c537
Adrian Ayala Gomez <adrian.ayala@redabogacia.org>
2017-07-17 09:37:53 +0000

    126_003 - Ejecucion de scripts en Integracion

--- bd/Entregas/SIGA_126_XXX.sql
+++ bd/Entregas/SIGA_126_XXX.sql
@@ -1,2 +1,5 @@
 Carpeta con script SIGA_126_002, aunque ahora es para la 126_003, porque hubo una entrega urgente anteriormente.
 
+-- Ejecutados en Integracion por AAG el 17/07/2017 a las 11:35
+
+
