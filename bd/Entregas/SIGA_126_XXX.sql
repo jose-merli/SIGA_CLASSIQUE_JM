c62814c233057a6eac642030ebe911e658bbcbe6
Adrian Ayala Gomez <adrian.ayala@redabogacia.org>
2017-07-17 13:39:41 +0000

    126_003 - Ejecucion de scripts en Integracion

--- bd/Entregas/SIGA_126_XXX.sql
+++ bd/Entregas/SIGA_126_XXX.sql
@@ -4,4 +4,6 @@ Carpeta con script SIGA_126_002, aunque ahora es para la 126_003, porque hubo un
 
 UPDATE ECOM_CEN_EX_COLUMN C SET C.NORMALIZACION = 'DOÑA.\s|D\.\s|Dº\s|D\u00aa\s|\p{Punct}|^(\u00A0)|(\u00A0)$' WHERE C.NOM_COL = 'NOMBRE';
 UPDATE ECOM_CEN_EX_COLUMN C SET C.NORMALIZACION = '[[\p{Punct}]&&[-]&&[.]]|^(\u00A0)|(\u00A0)$' WHERE C.NOM_COL IN ('APELLIDO1', 'APELLIDO2');
-commit;
+
+-- Ejecutados en Integracion por AAG el 17/07/2017 a las 15:35
+
