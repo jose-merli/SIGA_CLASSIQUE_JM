64da930ba06bb4e6bed5e58b4e3accd90796bd2d
Jorge Torres Acosta <jorge.torres@redabogacia.org>
2017-11-29 08:22:19 +0000

    SIGA_126_13 R1711_0024 No hacer join de SCS_EEJG_PETICIONES con SCS_EJG y sacar el el único dato que se obtiene de esa tabla de un select dentro de los campos de salida. Se ha tuneado añadiendo enlace a SCS_EEJG_PETICIONES

--- bd/Entregas/SIGA_126_XXX.sql
+++ bd/Entregas/SIGA_126_XXX.sql
@@ -412,3 +412,4 @@ update adm_tiposacceso hijo set hijo.derechoacceso  = (select padre.derechoacces
 
 --126_012: Ejecutados en Integracion por AAG el 27/11/2017 a las 09:30
 
+--eJECUTAR V_WS_2064_EJG
