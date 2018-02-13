049b8de0ee786430a0e926a5530e97da6ab882ff
Adrian Ayala Gomez <adrian.ayala@redabogacia.org>
2018-02-13 15:47:33 +0000

    127_001 - Ejecucion de scripts

--- bd/Entregas/SIGA_127_001.sql
+++ bd/Entregas/SIGA_127_001.sql
@@ -2,3 +2,5 @@ SIGA_127_001\R1709_0035.sql
 SIGA_127_001\R1709_0035.carga.historico.facturas.sql
 
 SIGA_127_001\R1610_0014.sql
+
+--127_001: Ejecutados en Integracion por AAG el 13/02/2018 a las 16:45
