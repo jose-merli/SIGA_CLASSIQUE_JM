0a50f71b2bdb9b8c5155204492558bb935ef3561
Jorge Borrego <jborrego@deloitte.es>
2019-06-21 12:03:34 +0000

    CENSO-171-Comunicar cambio de documentación colegiados en sincronización censo

---
+++ bd/Entregas/SIGA_128_XXX.sql
@@ -0,0 +1,32 @@
+--Tabla para almacenar los cambios de documento de los colegiados.
+create table CEN_HISTORICO_DOCPERSONA
+(
+  IDCENHISTORICODOC NUMBER(10) not null,
+  IDPERSONA        NUMBER(10) not null,
+  IDINSTITUCION    NUMBER(4) not null,
+  TIPOIDENTIFICACIONNEW    NUMBER(2) not null,
+  NUMEROIDENTIFICACIONNEW  VARCHAR2(20) not null,	
+  TIPOIDENTIFICACIONOLD    NUMBER(2) not null,
+  NUMEROIDENTIFICACIONOLD  VARCHAR2(20) not null,	
+  USUMODIFICACION          NUMBER(4) not null,
+  FECHAMODIFICACION DATE not null
+)tablespace TS_SIGA_CMN;
+
+alter table CEN_HISTORICO_DOCPERSONA
+  add constraint PK_CENHIST_DOC primary key (IDCENHISTORICODOC)
+  using index 
+  tablespace TS_SIGA_CMN_IDX;
+  
+  alter table cen_historico_docpersona
+  add constraint FK_CEN_HISTORICO_PERSONA foreign key (IDPERSONA)
+  references CEN_PERSONA ( IDPERSONA);
+ 
+alter table cen_historico_docpersona
+  add constraint FK_CEN_HISTORICO_INSTITUCION foreign key (IDINSTITUCION)
+  references CEN_INSTITUCION (IDINSTITUCION);
+
+create sequence SEQ_CEN_HISTORICO_DOCPERSONA minvalue 1 maxvalue 9999999999 start with 1 increment by 1 nocache;
+
+GRANT SELECT ON CEN_HISTORICO_DOCPERSONA TO ROLE_SIGA_R;
+GRANT SELECT, INSERT, UPDATE, DELETE ON CEN_HISTORICO_DOCPERSONA TO ROLE_SIGA;
+
