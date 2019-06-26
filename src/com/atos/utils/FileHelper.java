f8c4715f1278fdf615172d53300a2baa19e1c016
Adrian Ayala Gomez <adrian.ayala@redabogacia.org>
2019-06-26 08:28:28 +0000

    SIGARNV-1092 - Arreglo de creacion de directorios (mkdirs) para intentar disminuir la probabilidad de bloqueos en SIGA. Solo damos permisos cuando se crea una carpeta nueva. Esto ocurrira poco, asi que en caso de bloqueo, no desbordara al sistema. Gracias, Jose, por la idea

--- src/com/atos/utils/FileHelper.java
+++ src/com/atos/utils/FileHelper.java
@@ -8,21 +8,21 @@ import org.redabogacia.sigaservices.app.util.SIGAReferences;
 public class FileHelper
 {
 	private static final String SIGArootPath = new ReadProperties(SIGAReferences.RESOURCE_FILES.SIGA).returnProperty("directorios.path.OrigenPlantillas");
-	private static void addPerm777(File file) {
+	private static void addPerm777(File file, boolean changeParentsToo) {
 		if (file != null && file.exists()) {
 			try {
-				// Permissions to the parent file
-				File parentFile = file.getParentFile();
-				if (parentFile != null && parentFile.exists() && !parentFile.getAbsolutePath().equalsIgnoreCase(SIGArootPath)) {
-					addPerm777(parentFile);
-				}
-				
 				// Permissions to this file
-				ClsLogging.writeFileLogWithoutSession("Cambiando los permisos de: " + parentFile.getAbsolutePath());
+				ClsLogging.writeFileLogWithoutSession("Cambiando los permisos de: " + file.getAbsolutePath());
 				file.setReadable(true, false);
 				file.setWritable(true, false);
 				file.setExecutable(true, false);
-				ClsLogging.writeFileLogWithoutSession("OK - Permisos cambiados a: " + parentFile.getAbsolutePath());
+				ClsLogging.writeFileLogWithoutSession("OK - Permisos cambiados a: " + file.getAbsolutePath());
+				
+				// Permissions to the parent file
+				File parentFile = file.getParentFile();
+				if (changeParentsToo && parentFile != null && parentFile.exists() && !parentFile.getAbsolutePath().equalsIgnoreCase(SIGArootPath)) {
+					addPerm777(parentFile, changeParentsToo);
+				}
 			} catch (Exception e) {
 				ClsLogging.writeFileLogWithoutSession("Error al cambiar los permisos del fichero " + file.getAbsolutePath() + " - " + e.toString());
 			}
@@ -32,17 +32,20 @@ public class FileHelper
 	/**
 	 * Checks if the given file path exists. If not, it calls File.mkdirs(). 
 	 * Then, adds all ther permisions (777 on Linux) to the directory named by this abstract pathname, including any necessary parent directories. 
-	 * Note that this operation fails when the system user does not have permissions to do thant on some parent, but it will have been succeeded in giving permissions backwards up to that point.
+	 * Note that this operation fails when the system user does not have permissions to do that on some parent, but it will have been succeeded in giving permissions backwards up to that point.
 	 * @param filePath
 	 */
 	public static void mkdirs (String filePath) {
+		boolean changeParentsToo;
+		
 		if (filePath == null || filePath.trim().equalsIgnoreCase(""))
 			return;
 		
 		File fileDir = new File(filePath.toString());
 		if(fileDir!=null && !fileDir.exists()) {
+			changeParentsToo = !fileDir.getParentFile().exists(); 
 			fileDir.mkdirs();
-			addPerm777(fileDir);
+			addPerm777(fileDir, changeParentsToo);
 		}
 	}
 }
