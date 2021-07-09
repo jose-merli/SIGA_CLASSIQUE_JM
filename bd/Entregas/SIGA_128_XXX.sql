f9818c248e71e123c960671c20f5abc5023f9d8a
Adrian Ayala Gomez <adrian.ayala@redabogacia.org>
2021-07-09 10:56:59 +0000

    https://redabogacia.atlassian.net/browse/IOJ-7467: Aumentamos el tamaño del campo de identificación, que se ha llegado al límite en los colegios de Córdoba y Jaen.

--- bd/Entregas/SIGA_128_XXX.sql
+++ bd/Entregas/SIGA_128_XXX.sql
@@ -1151,8 +1151,10 @@ INSERT into CEN_GRUPOSINSTITUCION_INSTITU(idgrupo, idinstitucion, fechamodificac
 INSERT into CEN_GRUPOSINSTITUCION_INSTITU(idgrupo, idinstitucion, fechamodificacion, usumodificacion, fecha_inicio) values(1,	2067	, sysdate, 0, '01/01/2000');
 INSERT into CEN_GRUPOSINSTITUCION_INSTITU(idgrupo, idinstitucion, fechamodificacion, usumodificacion, fecha_inicio) values(1,	2017	, sysdate, 0, '01/01/2000');
 
-
 --SIGA_553
 CEN_COLEGIADO_BU
 CEN_COLEGIADO_AU
+
+--Ya ejecutado en Produccion con caracter de urgencia: 
+ALTER TABLE SCS_DOCUMENTACIONDESIGNA MODIFY IDDOCUMENTACIONDES NUMBER(10,0);
 
