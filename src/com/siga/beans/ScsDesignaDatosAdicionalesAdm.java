8cc23944a07b13293ebf0ede60e0fd26dffcaa46
Adrian Ayala Gomez <adrian.ayala@redabogacia.org>
2024-03-07 16:35:37 +0000

    https://redabogacia.atlassian.net/browse/SIGA-846

---
+++ src/com/siga/beans/ScsDesignaDatosAdicionalesAdm.java
@@ -0,0 +1,202 @@
+
+package com.siga.beans;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Vector;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.transaction.UserTransaction;
+
+import org.apache.struts.action.ActionMapping;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.atos.utils.ClsConstants;
+import com.atos.utils.ClsExceptions;
+import com.atos.utils.ComodinBusquedas;
+import com.atos.utils.GstDate;
+import com.atos.utils.Row;
+import com.atos.utils.RowsContainer;
+import com.atos.utils.UsrBean;
+import com.siga.Utilidades.PaginadorBind;
+import com.siga.Utilidades.UtilidadesBDAdm;
+import com.siga.Utilidades.UtilidadesHash;
+import com.siga.Utilidades.UtilidadesString;
+import com.siga.general.MasterForm;
+import com.siga.general.SIGAException;
+import com.siga.gratuita.form.MaestroDesignasForm;
+
+/**
+ * Implementa las operaciones sobre la base de datos, es decir: select, insert, update... a la tabla SCS_DESIGNA_DATOSADICIONALES
+ * 
+ * @since 07/03/2024 
+ */
+public class ScsDesignaDatosAdicionalesAdm extends MasterBeanAdministrador {
+
+
+	/**
+	 * Constructor de la clase. 
+	 * 
+	 * @param usuario Usuario "logado" en la aplicación. De tipo "Integer".  
+	 */
+	public ScsDesignaDatosAdicionalesAdm (UsrBean usuario) {
+		super( ScsDesignaDatosAdicionalesBean.T_NOMBRETABLA, usuario);
+	}
+
+	/** Funcion getCamposBean ()
+	 *  @return conjunto de datos con los nombres de todos los campos del bean
+	 * 
+	 */
+	protected String[] getCamposBean() {
+		String[] campos = {
+				ScsDesignaDatosAdicionalesBean.C_IDINSTITUCION 						 ,
+				ScsDesignaDatosAdicionalesBean.C_IDTURNO                             ,
+				ScsDesignaDatosAdicionalesBean.C_ANIO                                ,
+				ScsDesignaDatosAdicionalesBean.C_NUMERO                              ,
+				ScsDesignaDatosAdicionalesBean.C_NUMEROASUNTO                        ,
+				ScsDesignaDatosAdicionalesBean.C_FECHAMODIFICACION                   ,
+				ScsDesignaDatosAdicionalesBean.C_USUMODIFICACION                     ,
+				ScsDesignaDatosAdicionalesBean.C_INICIO_PROCESO                      ,
+				ScsDesignaDatosAdicionalesBean.C_TIPO_AUTO                           ,
+				ScsDesignaDatosAdicionalesBean.C_FECHA_RESOLUCION_JUDICIAL           ,
+				ScsDesignaDatosAdicionalesBean.C_FECHA_RESOLUCION_JUDICIAL_OPOSICION ,
+				ScsDesignaDatosAdicionalesBean.C_FECHA_ESCRITURA                     ,
+				ScsDesignaDatosAdicionalesBean.C_FECHA_RESOLUCION_SENTENCIA_FIRME    ,
+				ScsDesignaDatosAdicionalesBean.C_NUMERO_VISTAS_ADICIONALES           ,
+				ScsDesignaDatosAdicionalesBean.C_FECHA_VISTA                         ,
+				ScsDesignaDatosAdicionalesBean.C_FECHA_REQUERIMIENTO_JUDICIAL        ,
+				ScsDesignaDatosAdicionalesBean.C_NUMERO_PERSONADOS_MACROCAUSA        ,
+				ScsDesignaDatosAdicionalesBean.C_ESVICTIMA                           ,
+				ScsDesignaDatosAdicionalesBean.C_ESSUSTITUCION                       
+		};
+		return campos;
+	}
+	/** Funcion getClavesBean ()
+	 *  @return conjunto de datos con los nombres de todos los campos que forman la claves del bean
+	 * 
+	 */
+	protected String[] getClavesBean() {
+		String[] campos = {	
+				ScsDesignaDatosAdicionalesBean.C_IDINSTITUCION,			
+				ScsDesignaDatosAdicionalesBean.C_IDTURNO,
+				ScsDesignaDatosAdicionalesBean.C_ANIO,					
+				ScsDesignaDatosAdicionalesBean.C_NUMERO,
+				ScsDesignaDatosAdicionalesBean.C_NUMEROASUNTO
+		};
+		return campos;
+	}
+
+	
+	/** Funcion hashTableToBean (Hashtable hash)
+	 *  @param hash Hashtable para crear el bean
+	 *  @return bean con la información de la hashtable
+	 * 
+	 */
+	public MasterBean hashTableToBean(Hashtable hash) throws ClsExceptions {
+		ScsDesignaDatosAdicionalesBean bean = null;
+		try{
+			bean = new ScsDesignaDatosAdicionalesBean();
+			bean.setAnio(UtilidadesHash.getInteger(hash,ScsDesignaDatosAdicionalesBean.C_ANIO));
+			bean.setIdInstitucion(UtilidadesHash.getInteger(hash,ScsDesignaDatosAdicionalesBean.C_IDINSTITUCION));
+			bean.setIdTurno(UtilidadesHash.getInteger(hash,ScsDesignaDatosAdicionalesBean.C_IDTURNO));
+			bean.setNumero(UtilidadesHash.getLong(hash,ScsDesignaDatosAdicionalesBean.C_NUMERO));
+			bean.setNumeroAsunto(UtilidadesHash.getLong(hash,ScsDesignaDatosAdicionalesBean.C_NUMEROASUNTO));
+			bean.setFechaMod(UtilidadesHash.getString(hash,ScsDesignaDatosAdicionalesBean.C_FECHAMODIFICACION));
+			bean.setUsuMod(UtilidadesHash.getInteger(hash,ScsDesignaDatosAdicionalesBean.C_USUMODIFICACION));	
+			bean.setInicioProceso(UtilidadesHash.getString(hash,ScsDesignaDatosAdicionalesBean.C_INICIO_PROCESO                      ));
+			bean.setTipoAuto(UtilidadesHash.getInteger(hash,ScsDesignaDatosAdicionalesBean.C_TIPO_AUTO                           ));
+			bean.setFechaResolucionJudicial(UtilidadesHash.getString(hash,ScsDesignaDatosAdicionalesBean.C_FECHA_RESOLUCION_JUDICIAL           ));
+			bean.setFechaResolucionJudicialOposicion(UtilidadesHash.getString(hash,ScsDesignaDatosAdicionalesBean.C_FECHA_RESOLUCION_JUDICIAL_OPOSICION ));
+			bean.setFechaEscritura(UtilidadesHash.getString(hash,ScsDesignaDatosAdicionalesBean.C_FECHA_ESCRITURA                     ));
+			bean.setFechaResolucionSentenciaFirme(UtilidadesHash.getString(hash,ScsDesignaDatosAdicionalesBean.C_FECHA_RESOLUCION_SENTENCIA_FIRME    ));
+			bean.setNumeroVistasAdicionales(UtilidadesHash.getInteger(hash,ScsDesignaDatosAdicionalesBean.C_NUMERO_VISTAS_ADICIONALES           ));
+			bean.setFechaVista(UtilidadesHash.getString(hash,ScsDesignaDatosAdicionalesBean.C_FECHA_VISTA                         ));
+			bean.setFechaRequerimientoJudicial(UtilidadesHash.getString(hash,ScsDesignaDatosAdicionalesBean.C_FECHA_REQUERIMIENTO_JUDICIAL        ));
+			bean.setNumeroPersonadosMacrocausa(UtilidadesHash.getInteger(hash,ScsDesignaDatosAdicionalesBean.C_NUMERO_PERSONADOS_MACROCAUSA        ));
+			bean.setEsVictima(UtilidadesHash.getString(hash,ScsDesignaDatosAdicionalesBean.C_ESVICTIMA                           ));
+			bean.setEsSustitucion(UtilidadesHash.getString(hash,ScsDesignaDatosAdicionalesBean.C_ESSUSTITUCION                       ));
+		}
+		catch(Exception e){
+			bean = null;
+			throw new ClsExceptions (e, "Error al construir el bean a partir del hashTable");
+		}
+		return bean;
+	}
+
+	/** Funcion beanToHashTable (MasterBean bean)
+	 *  @param bean para crear el hashtable asociado
+	 *  @return hashtable con la información del bean
+	 * 
+	 */
+	protected Hashtable beanToHashTable(MasterBean bean) throws ClsExceptions {
+		Hashtable hash = null;
+		try{
+			hash = new Hashtable();
+			ScsDesignaDatosAdicionalesBean b = (ScsDesignaDatosAdicionalesBean) bean;
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_IDINSTITUCION,b.getIdInstitucion());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_IDTURNO,b.getIdTurno());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_ANIO,b.getAnio());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_NUMERO,b.getNumero());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_NUMEROASUNTO,b.getNumeroAsunto());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_FECHAMODIFICACION,b.getFechaMod());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_USUMODIFICACION,b.getUsuMod());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_INICIO_PROCESO,b.getInicioProceso());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_TIPO_AUTO,b.getTipoAuto());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_FECHA_RESOLUCION_JUDICIAL,b.getFechaResolucionJudicial());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_FECHA_RESOLUCION_JUDICIAL_OPOSICION,b.getFechaResolucionJudicialOposicion());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_FECHA_ESCRITURA,b.getFechaEscritura());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_FECHA_RESOLUCION_SENTENCIA_FIRME,b.getFechaResolucionSentenciaFirme());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_NUMERO_VISTAS_ADICIONALES,b.getNumeroVistasAdicionales());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_FECHA_VISTA,b.getFechaVista());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_FECHA_REQUERIMIENTO_JUDICIAL,b.getFechaRequerimientoJudicial());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_NUMERO_PERSONADOS_MACROCAUSA,b.getNumeroPersonadosMacrocausa());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_ESVICTIMA,b.getEsVictima());
+			UtilidadesHash.set(hash, ScsDesignaDatosAdicionalesBean.C_ESSUSTITUCION,b.getEsSustitucion());
+			return hash;
+		}
+		catch (Exception e){
+			hash = null;
+			throw new ClsExceptions (e, "Error al construir el hashTable a partir del bean");			
+		}
+	}
+
+	/** Funcion getOrdenCampos ()
+	 *  @return String[] conjunto de valores con los campos por los que se deberá ordenar la select
+	 *  que se ejecute sobre esta tabla
+	 */
+	protected String[] getOrdenCampos() {
+		return null;
+	}
+	
+	/** Funcion selectGenerico (String consulta). Ejecuta la consulta que se le pasa en un string 
+	 *  @return vector con los registros encontrados. El objeto es de tipo administrador del bean 
+	 * */
+	public Vector selectGenerico(String consulta) throws ClsExceptions 
+	{
+		Vector datos = new Vector();
+		
+		// Acceso a BBDD
+		RowsContainer rc = null;
+		try { 
+			rc = new RowsContainer();			
+
+			if (rc.query(consulta)) {
+				for (int i = 0; i < rc.size(); i++)	{
+					Row fila = (Row) rc.get(i);
+					Hashtable registro = (Hashtable) fila.getRow(); 
+					if (registro != null) 
+						datos.add(registro);
+				}
+			}
+		} 
+		catch (Exception e) { 	
+			throw new ClsExceptions (e, "Error al ejecutar el 'select' en B.D."); 
+		}
+		return datos;
+	}
+	
+}
---
+++ src/com/siga/beans/ScsDesignaDatosAdicionalesBean.java
@@ -0,0 +1,171 @@
+package com.siga.beans;
+
+/**
+ * Implementa las operaciones sobre el bean de la tabla SCS_DESIGNA_DATOSADICIONALES
+ * 
+ * @since 07/03/2024
+ */
+public class ScsDesignaDatosAdicionalesBean extends MasterBean {
+	
+	private static final long serialVersionUID = 8607899948216476084L;
+	// Variables 
+	private Integer	idInstitucion ;
+	private Integer	idTurno ;
+	private Integer	anio ;
+	private Long	numero ;
+	private Long	numeroAsunto ;
+	private String	fechaModificacion ;
+	private Integer	usuModificacion ;
+	private String	inicioProceso ;
+	private Integer	tipoAuto ;
+	private String	fechaResolucionJudicial ;
+	private String	fechaResolucionJudicialOposicion ;
+	private String	fechaEscritura ;
+	private String	fechaResolucionSentenciaFirme ;
+	private Integer	numeroVistasAdicionales ;
+	private String	fechaVista ;
+	private String	fechaRequerimientoJudicial ;
+	private Integer	numeroPersonadosMacrocausa ;
+	private String	esVictima ;
+	private String	esSustitucion ;
+	
+	// Nombre de Tabla
+	static public String T_NOMBRETABLA = "SCS_DESIGNA_DATOSADICIONALES";
+	
+	// Nombre de campos de la tabla
+	static public final String	C_IDINSTITUCION 						=  "IDINSTITUCION";
+	static public final String	C_IDTURNO                               =  "IDTURNO";
+	static public final String	C_ANIO                                  =  "ANIO";
+	static public final String	C_NUMERO                                =  "NUMERO";
+	static public final String	C_NUMEROASUNTO                          =  "NUMEROASUNTO";
+	static public final String	C_FECHAMODIFICACION                     =  "FECHAMODIFICACION";
+	static public final String	C_USUMODIFICACION                       =  "USUMODIFICACION";
+	static public final String	C_INICIO_PROCESO                        =  "INICIO_PROCESO";
+	static public final String	C_TIPO_AUTO                             =  "TIPO_AUTO";
+	static public final String	C_FECHA_RESOLUCION_JUDICIAL             =  "FECHA_RESOLUCION_JUDICIAL";
+	static public final String	C_FECHA_RESOLUCION_JUDICIAL_OPOSICION   =  "FECHA_RESOLUCION_JUDICIAL_OPOSICION";
+	static public final String	C_FECHA_ESCRITURA                       =  "FECHA_ESCRITURA";
+	static public final String	C_FECHA_RESOLUCION_SENTENCIA_FIRME      =  "FECHA_RESOLUCION_SENTENCIA_FIRME";
+	static public final String	C_NUMERO_VISTAS_ADICIONALES             =  "NUMERO_VISTAS_ADICIONALES";
+	static public final String	C_FECHA_VISTA                           =  "FECHA_VISTA";
+	static public final String	C_FECHA_REQUERIMIENTO_JUDICIAL          =  "FECHA_REQUERIMIENTO_JUDICIAL";
+	static public final String	C_NUMERO_PERSONADOS_MACROCAUSA          =  "NUMERO_PERSONADOS_MACROCAUSA";
+	static public final String	C_ESVICTIMA                             =  "ESVICTIMA";
+	static public final String	C_ESSUSTITUCION                         =  "ESSUSTITUCION";
+	
+	public Integer getIdInstitucion() {
+		return idInstitucion;
+	}
+	public void setIdInstitucion(Integer idInstitucion) {
+		this.idInstitucion = idInstitucion;
+	}
+	public Integer getIdTurno() {
+		return idTurno;
+	}
+	public void setIdTurno(Integer idTurno) {
+		this.idTurno = idTurno;
+	}
+	public Integer getAnio() {
+		return anio;
+	}
+	public void setAnio(Integer anio) {
+		this.anio = anio;
+	}
+	public Long getNumero() {
+		return numero;
+	}
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+	public Long getNumeroAsunto() {
+		return numeroAsunto;
+	}
+	public void setNumeroAsunto(Long numeroAsunto) {
+		this.numeroAsunto = numeroAsunto;
+	}
+	public String getFechaModificacion() {
+		return fechaModificacion;
+	}
+	public void setFechaModificacion(String fechaModificacion) {
+		this.fechaModificacion = fechaModificacion;
+	}
+	public Integer getUsuModificacion() {
+		return usuModificacion;
+	}
+	public void setUsuModificacion(Integer usuModificacion) {
+		this.usuModificacion = usuModificacion;
+	}
+	public String getInicioProceso() {
+		return inicioProceso;
+	}
+	public void setInicioProceso(String inicioProceso) {
+		this.inicioProceso = inicioProceso;
+	}
+	public Integer getTipoAuto() {
+		return tipoAuto;
+	}
+	public void setTipoAuto(Integer tipoAuto) {
+		this.tipoAuto = tipoAuto;
+	}
+	public String getFechaResolucionJudicial() {
+		return fechaResolucionJudicial;
+	}
+	public void setFechaResolucionJudicial(String fechaResolucionJudicial) {
+		this.fechaResolucionJudicial = fechaResolucionJudicial;
+	}
+	public String getFechaResolucionJudicialOposicion() {
+		return fechaResolucionJudicialOposicion;
+	}
+	public void setFechaResolucionJudicialOposicion(String fechaResolucionJudicialOposicion) {
+		this.fechaResolucionJudicialOposicion = fechaResolucionJudicialOposicion;
+	}
+	public String getFechaEscritura() {
+		return fechaEscritura;
+	}
+	public void setFechaEscritura(String fechaEscritura) {
+		this.fechaEscritura = fechaEscritura;
+	}
+	public String getFechaResolucionSentenciaFirme() {
+		return fechaResolucionSentenciaFirme;
+	}
+	public void setFechaResolucionSentenciaFirme(String fechaResolucionSentenciaFirme) {
+		this.fechaResolucionSentenciaFirme = fechaResolucionSentenciaFirme;
+	}
+	public Integer getNumeroVistasAdicionales() {
+		return numeroVistasAdicionales;
+	}
+	public void setNumeroVistasAdicionales(Integer numeroVistasAdicionales) {
+		this.numeroVistasAdicionales = numeroVistasAdicionales;
+	}
+	public String getFechaVista() {
+		return fechaVista;
+	}
+	public void setFechaVista(String fechaVista) {
+		this.fechaVista = fechaVista;
+	}
+	public String getFechaRequerimientoJudicial() {
+		return fechaRequerimientoJudicial;
+	}
+	public void setFechaRequerimientoJudicial(String fechaRequerimientoJudicial) {
+		this.fechaRequerimientoJudicial = fechaRequerimientoJudicial;
+	}
+	public Integer getNumeroPersonadosMacrocausa() {
+		return numeroPersonadosMacrocausa;
+	}
+	public void setNumeroPersonadosMacrocausa(Integer numeroPersonadosMacrocausa) {
+		this.numeroPersonadosMacrocausa = numeroPersonadosMacrocausa;
+	}
+	public String getEsVictima() {
+		return esVictima;
+	}
+	public void setEsVictima(String esVictima) {
+		this.esVictima = esVictima;
+	}
+	public String getEsSustitucion() {
+		return esSustitucion;
+	}
+	public void setEsSustitucion(String esSustitucion) {
+		this.esSustitucion = esSustitucion;
+	}
+	
+}
