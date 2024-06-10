77a68ffaea70a8f15bb36076ea8813a994680a90
Adrian Ayala Gomez <adrian.ayala@redabogacia.org>
2024-06-10 15:01:45 +0000

    Arreglo urgente de documentacion EJG

--- src/com/siga/beans/ScsDocumentacionEJGAdm.java
+++ src/com/siga/beans/ScsDocumentacionEJGAdm.java
@@ -606,7 +606,7 @@ public class ScsDocumentacionEJGAdm extends MasterBeanAdministrador {
 					      "                    AND SCS_PARENTESCO.IDPARENTESCO = " +
 					      "                        FAMILIA.IDPARENTESCO)) || ')') AS DESCPRESENTADOR, " +
 					      
-						  "          SELECT F_SIGA_GETRECURSO(D.ABREVIATURA, " + (String)params.get("codIdioma") +") DOCUMENTOABREVIATURA" +
+						  "          F_SIGA_GETRECURSO(D.ABREVIATURA, " + (String)params.get("codIdioma") +") DOCUMENTOABREVIATURA" +
 					      "  	,DE.COMISIONAJG " ;
 					      
 				if(isColegioConfiguradoEnvioCAJG) {	      
